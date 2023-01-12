c2988ae6d88936d5899c7ed890d09930ac7a9902
Justin <justin2004@hotmail.com>
2023-01-11 22:32:35 -0500

    adding rendering support
    using the Dyalog APL disp and display functions

--- apl.sh
+++ apl.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-user_function=$1
-first=$2
-second=$3
+if [ $1 = -r ] # r as in render
+then
+    script+="'display' 'disp'⎕CY'dfns' ⋄"
+    render=$2
+    user_function=$3
+    first=$4
+    second=$5
+else
+    render=""
+    user_function=$1
+    first=$2
+    second=$3
+fi
 
 if [ -z "$user_function" ]
 then
     echo ERROR: TODO put a helpful usage message here
     exit 1
 fi
-# script+="⍎']box'⋄"
-# script+="]boxing on ⋄"
 
 if [ ! -z "$first" ]
 then
@@ -26,14 +34,14 @@ then
             second="/dev/stdin"
         fi
         script+="secondargument←⊃⎕NGET"\'$second\'" 1 ⋄"
-        script+="⎕←firstargument ($user_function) secondargument"
+        script+="⎕←$render firstargument ($user_function) secondargument"
     else
-        script+="⎕←($user_function) firstargument"
+        script+="⎕←$render ($user_function) firstargument"
     fi
 else
     # first="/dev/stdin"
     # script+="firstargument←⊃⎕NGET"\'$first\'" 1 ⋄"
-    script+="⎕←$user_function"
+    script+="⎕←$render $user_function"
 fi
 
 # echo script is
